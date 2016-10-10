8992006b96c4f633891c215735f9521e76f21ead
bayernziyan <bayern_ziyan@163.com>
2016-10-10 16:05:07 +0800

    Change-Id: I1f8edf301d04679710eb3fe24c830b001173443d

---
+++ exttool/src/main/java/com/eccm/ext/tools/workflow/handler/GetFormDataValuesByItemExt1Singly.java
@@ -0,0 +1,61 @@
+package com.eccm.ext.tools.workflow.handler;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.eccm.ext.tools.util.WorkflowUtil;
+import com.eccm.ext.tools.workflow.ParamTranslator;
+import com.eccm.ext.tools.workflow.WorkflowAction;
+import com.eccm.ext.tools.workflow.WorkflowActionHandler;
+/**
+ * 
+ * @author bayern
+ * <pre>基于当前流程，通过自定义标识 获取对应的参数值</pre>
+ * 
+ * 	  @param param_in_trigger 其他输入参数之前读取，如果存在执行ParamTranslator 生成输入参数
+ * 	  @param param_in_list 为自定义标识列表
+ * 	  @return param_out_map 为自定义标识 和 对应参数值的映射
+ */
+public class GetFormDataValuesByItemExt1Singly extends WorkflowActionHandler {
+	private static final Logger LOG = Logger.getLogger(GetFormDataValuesByItemExt1Singly.class);
+	
+	public static final String name = "get_form_data_itemext1_single";	
+	
+	public static final String param_in_trigger = name+"_in_trigger";
+	public static final String param_in_list = name+"_in_list";
+	/**
+	 *  HashMap&lt;String,String&gt;
+	 */
+	public static final String param_out_map = name+"_out_getformdata_byext1_hashmap";
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public void doHandler(WorkflowAction action,Connection conn) {
+		Object _trigger = getArg(action, param_in_trigger);
+		if(null!=_trigger && _trigger instanceof ParamTranslator)
+			((ParamTranslator)_trigger).translate();
+		Object _list  =  getArg(action, param_in_list);		
+		try{	
+			if( null == _list ) throw new IllegalArgumentException("参数为空");
+			LOG.info("getFormDataValueByItemExt1List==============");
+			HashMap<String,String> map = WorkflowUtil.getFormDataValueByItemExt1List(conn,(List<String>)_list, action.getWfBean().getFormDataId(),action.getWfBean().getFormId());
+			LOG.info("getFormDataValueByItemExt1List=============="+(null == map));
+			if(null != map) {
+				setArg(action, param_out_map, map);
+			}			
+		}catch(Exception e){setException(e);LOG.error(e);}
+	}
+	
+	public GetFormDataValuesByItemExt1Singly(){}
+	public GetFormDataValuesByItemExt1Singly(HashMap<String,ArrayList<String>> argRelationShip){
+		this.argRelationShip = argRelationShip;
+	}
+}
